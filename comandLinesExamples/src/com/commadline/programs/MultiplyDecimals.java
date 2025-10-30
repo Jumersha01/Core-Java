00e1d394cbaf14e7b7c2800cfdb1d6b3822e1ad3
Jumersha01 <www.junaid555fakir@gmail.com>
2025-10-29 12:40:32 -1200

    added comment

---
+++ comandLinesExamples/src/com/commadline/programs/MultiplyDecimals.java
@@ -0,0 +1,13 @@
+package com.commadline.programs;
+
+public class MultiplyDecimals {
+
+	public static void main(String[] args) {
+		//this is a solution
+		double num1=Double.parseDouble(args[0]);
+		double num2=Double.parseDouble(args[1]);
+		double product= num1*num2;
+		System.out.println("Multiplication Result:"+product);
+	}
+
+}
