eabfe7c114115c3c60371c0a7603a14df73b3f7f
Jumersha01 <www.junaid555fakir@gmail.com>
2025-11-04 19:10:48 -1200

    removing the duplicate element from array

---
+++ Arrays_Basics/src/com/arrays/day_4/RemoveDuplicate.java
@@ -0,0 +1,31 @@
+package com.arrays.day_4;
+
+import java.util.*;
+public class RemoveDuplicate {
+	
+	
+	    public static void main (String [] args){
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter the Size of an Array");
+	        int n = sc.nextInt();
+	        if(n<=0){
+	            System.out.println("Invalid Array Size");
+	            System.exit(0);
+	        }
+	        int arr [] = new int[n];
+	        
+	         
+	          for(int i=0;i<n;i++){
+	        	   System.out.print("Enter the "+i+"th Element");
+	            arr[i] = sc.nextInt();
+	          }
+	         arr= Arrays.stream(arr).distinct().toArray();
+	         System.out.print("Array after removing duplicates: "+Arrays.toString(arr));
+	       
+
+
+
+	    }
+	 }
+
+
