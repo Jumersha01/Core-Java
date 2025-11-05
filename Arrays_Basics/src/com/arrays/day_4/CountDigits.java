9db55bd6c7769e0860108bcace20fb6fef6d2555
Jumersha01 <www.junaid555fakir@gmail.com>
2025-11-04 18:38:39 -1200

    counts Digits of array elements

---
+++ Arrays_Basics/src/com/arrays/day_4/CountDigits.java
@@ -0,0 +1,33 @@
+package com.arrays.day_4;
+
+import java.util.*;
+public class CountDigits {
+
+	
+	
+	    public static void main(String [] args){
+	    	
+	        Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the size of the Array:");
+	        int n = sc.nextInt();
+	        
+	        int arr [] = new int[n];
+	        for(int i =0;i<n;i++){
+	        	System.out.println("Enter the "+i+"th element");
+	            arr[i] = sc.nextInt();
+	        }
+	         for(int j = 0;j<arr.length;j++){
+	            int count =0;
+	            int temp = arr[j];
+	            while(temp!=0){
+	                temp/=10;
+	                count++;
+
+	            }
+	            System.out.println("Number: "+arr[j]+", Digits: "+count);
+	          
+	         }
+	         
+	    }
+	}
+
