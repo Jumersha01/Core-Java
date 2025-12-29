e0bfcc3fa1707116149bffc17dcdad1a562b009e
Jumersha01 <www.junaid555fakir@gmail.com>
2025-12-28 21:41:18 -1200

    finding max and min no in the array

---
+++ Logicals/src/com/junaid/array_logicals/MaxNumber.java
@@ -0,0 +1,31 @@
+package com.junaid.array_logicals;
+
+public class MaxNumber {
+	void main() {
+		int size = Integer.parseInt(IO.readln("Enter the size of the array: "));
+
+		int arr[] = new int[size];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(IO.readln("Enter the " + (i + 1) + "th element: "));
+		}
+
+		int max = arr[0];
+
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		IO.println("Max number found : " + max);
+		
+		int min =arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] <min) {
+				min = arr[i];
+			}
+		}
+		IO.println("Min number found : " + min);
+
+	}
+}
