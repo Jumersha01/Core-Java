c5421ee72f7fe6d48497bb13604c0ca0bf65af7a
Jumersha01 <www.junaid555fakir@gmail.com>
2026-01-28 23:14:50 -1200

    Replacing the Element

--- Logicals/src/com/junaid/array_logicals/ReplaceElement.java
+++ Logicals/src/com/junaid/array_logicals/ReplaceElement.java
@@ -15,7 +15,7 @@ public class ReplaceElement {
 		for (int i = 0; i < nums.length; i++) {
 			for (int j = i + 1; j < nums.length; j++) {
 				if(nums[i]==nums[j]) {
-					nums[i]=nums[j];
+					count++;
 				}
 			}
 			
