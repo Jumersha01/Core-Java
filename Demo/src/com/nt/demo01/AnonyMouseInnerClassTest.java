4cee809b8b71e75ce66187c94663f56587f67e21
JumershO1 <junaid555fakir@gmail.com>
2026-06-26 16:06:34 +0530

    Operations on  List using listiterator

--- Demo/src/com/nt/demo01/AnonyMouseInnerClassTest.java
+++ Demo/src/com/nt/demo01/AnonyMouseInnerClassTest.java
@@ -1,29 +1,45 @@
 package com.nt.demo01;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Predicate;
 
 public class AnonyMouseInnerClassTest {
  
 	void main() {
 		  Predicate<Integer> p = e->e%2==0;
-		  IO.println(p.test(10));
-		  List<Integer> list = List.of(1,2,3,4,5,6,7,8,9,10);
+		 
+		 // List<Integer> list = List.of(1,2,3,4,5,6,7,8,9,10);
+//		  
+//		  
+//		  class FindEvenOdd{
+//			
+//			  public void getEvenOdd(List<Integer> list) {
+//				  for(Integer l:list) {
+//				    boolean isEven = p.test(l);
+//				    String result= isEven? "Even NO is "+(l):"Odd NO is: "+(l);
+//				    System.out.println(result);
+//				  }
+//			  }
+//			
+//		  }
+//		  FindEvenOdd e = new  FindEvenOdd();
+//		  e.getEvenOdd(list);
 		  
+		  String str="Applee";
 		  
-		  class FindEvenOdd{
-			
-			  public void getEvenOdd(List<Integer> list) {
-				  for(Integer l:list) {
-				    boolean isEven = p.test(l);
-				    String result= isEven? "Even NO is "+(l):"Odd NO is: "+(l);
-				    System.out.println(result);
-				  }
-			  }
-			
-		  }
-		  FindEvenOdd e = new  FindEvenOdd();
-		  e.getEvenOdd(list);
+          Set<Character> seen = new HashSet<Character>();
+          Set<Character> duplicate = new HashSet<Character>();
+          
+          for(int i =0;i<str.length();i++) {
+        	      if(!seen.add(str.charAt(i))) {
+        	    	       duplicate.add(str.charAt(i));
+        	      }
+          }
+          duplicate.forEach(IO::println);
+          
+          
+		  
 		  
 		  
 	}
